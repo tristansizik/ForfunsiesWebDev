df70356cdc93311a24f0338623578dacbbcfe64c
Tristan Sizik <tristansizik@gmail.com>
2023-02-20 11:26:48 -0800

    removed errorboundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-// import ErrorBoundary from '@/components/skeleton/ErrorBoundary'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <ErrorBoundary>
       <Component {...pageProps} />
-    // </ErrorBoundary>
   )
 }
