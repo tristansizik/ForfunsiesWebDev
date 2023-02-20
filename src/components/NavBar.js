7e520d2ebebf8c7fc9420e62e67bd963d77f700b
Tristan Sizik <tristansizik@gmail.com>
2023-02-20 11:25:09 -0800

    fixing apostrophe compiler error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
           <NavBarIcon icon={<AiOutlineHome size="28" />} text={"Home!"} />
         </div>
         <div className="flex w-50% h-full px-0 justify-center items-center">
-          <div className="flex px-10 text-xlg text-slate-700 hover:text-white">Tristan's Website</div>
+          <div className="flex px-10 text-xlg text-slate-700 hover:text-white">Tristan&apos;s Website</div>
         </div>
         <div className="flex relative w-25% justify-">
           <div className="flex relative text-right right-10% w-25%">
