b3da37984ff4e4f6e3a6321cf1dc3f66aed544b4
Tristan Sizik <tristansizik@gmail.com>
2023-04-05 00:56:20 -0700

    adding movement to Box when you move mouse

--- src/components/sketches/ThreeJsSketch.tsx
+++ src/components/sketches/ThreeJsSketch.tsx
@@ -1,20 +1,37 @@
 import { Fragment, useRef, useState } from "react";
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, RootState, useFrame } from '@react-three/fiber';
+import { Scroll, ScrollControls, useScroll } from '@react-three/drei';
 import {Mesh} from 'three'
 
 const Box = (props : any) => {
   const ref = useRef<Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
+  const [position, setPosition] = useState(0);
 
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  const data = useScroll();
+  data.range = 0;
+  
+  // useFrame((state, delta) => (ref.current.rotation.x += delta));
+  useFrame( () => {
+              // ref.current.rotation.x += .01;
+              const a = data.range(0 , 1 / 3);
+  });
 
   return (
     <mesh
       {...props}
       ref={ref}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
+      onPointerMove={() => {
+        setPosition(prevPosition => {
+          ref.current.rotation.y = prevPosition;
+          return prevPosition -.01;
+        });
+      }}
+      onClick={(event) => {
+        setActive(!active);
+      }}
       onPointerOver = {(event) => setHover(true)}
       onPointerOut = {(event) => setHover(false)}
       >
@@ -32,7 +49,10 @@ export default function ThreeJsSketch() {
       <Canvas ref={canvasRef}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, -1, 0]} />
+        <Box position={[0, 0, 0]} />
+        <ScrollControls>
+          <Box position = {[0,0,0]} /> 
+        </ScrollControls>
       </Canvas>
     </Fragment>
   );
