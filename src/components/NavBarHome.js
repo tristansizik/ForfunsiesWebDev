ba5544260b7eeb15ff68cb11ad3326a50fc2a231
tristansizik <tristan.sizik@gmail.com>
2023-12-24 09:42:01 -0800

    started the responsive design of drop down box

--- src/components/NavBarHome.js
+++ src/components/NavBarHome.js
@@ -6,18 +6,19 @@ const NavBarHome = (bgColor) => {
   const [click, setClick] = useState(false);
     return (
       <>
-      <div className="justify-center items-center sticky top-0 h-20 w-full bg-faint-grey ">
-        <div className="flex h-full items-center">
-          <Link href="/" className="flex w-25% px-10 font-Hardigan font-bold text-green-blueish-dark hover:text-green-dark ">Home</Link>
-          <div className="flex w-50% h-full text-green-blueish-dark font-Hardigan text-2xl font-extrabold justify-center"></div>
-          <div className="flex w-25% font-Hardigan relative h-full justify-center">            
-            <div className="text-base leading-1 sm:flex hidden items-center">
-              <Link className="px-2 font-bold text-green-blueish-dark hover:text-green-dark" href="/threejspage">Blog</Link>
-              <Link className="px-2 font-bold text-green-blueish-dark hover:text-green-dark" href="/about">About</Link>
-              <Link className="px-2 font-bold text-green-blueish-dark hover:text-green-dark" href="/contact">Contact</Link>             
+      <div className="justify-center items-center sticky h-20 w-full bg-faint-grey ">
+        <div className="flex h-full items-center font-Hardigan font-bold text-green-blueish-dark">
+          <Link href="/" className="flex w-25% px-10 hover:text-green-dark ">Home</Link>
+          <div className="flex w-50% h-full font-extrabold items-center"></div>
+          <div className="w-25% h-full ">                        
+            <div className="absolute sm:relative flex-col md:flex-row gap-10 top-[-100%] sm:top-[inherit]"
+                 id="menu">
+              <Link className="hover:text-green-dark" href="/threejspage">Blog</Link>
+              <Link className="hover:text-green-dark" href="/about">About</Link>
+              <Link className="hover:text-green-dark" href="/contact">Contact</Link>             
             </div>
-            <div className='sm:hidden flex items-center'>
-              <NavBarIcon icon={<FaBars size="20" />} text={''} onclick={ClickEvent()}/>
+            <div className='sm:hidden items-center'>
+              <NavBarIcon icon={<FaBars size="20" />}/>
             </div>
           </div>
         </div>
@@ -26,19 +27,18 @@ const NavBarHome = (bgColor) => {
     );
   }
 
-  const NavBarIcon = ({icon, text = ''}) => {
+  const NavBarIcon = ({icon}) => {
     return (
-    <div className="navbar-icon group">
+    <button onClick={ClickEvent} className="navbar-icon group">
       {icon}
-      
-      <span className="navbar-tooltip">
-      </span>
-    </div>
+    </button>
     );
   }
 
   const ClickEvent = () => {
-    console.log("hi");
+    console.log("I've been clicked");
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("top-[50px]");
   }
 
   export default NavBarHome;
