59ce4fea3e70dc7f8c2602bed5f2482071c52b0c
tristansizik <tristan.sizik@gmail.com>
2023-12-30 11:05:43 -0800

    centered my items using grid, now need to fix the animation location + turn into row

--- src/components/NavBarHome.js
+++ src/components/NavBarHome.js
@@ -6,23 +6,24 @@ const NavBarHome = (bgColor) => {
   const [click, setClick] = useState(false);
     return (
       <>
-      <div className="justify-center items-center sticky h-20 w-full bg-faint-grey ">
-        <div className="flex h-full items-center font-Hardigan font-bold text-green-blueish-dark">
-          <Link href="/" className="flex w-25% px-10 hover:text-green-dark ">Home</Link>
-          <div className="flex w-50% h-full font-extrabold items-center"></div>
-          <div className="w-25% h-full">                        
-            <div className="absolute sm:relative flex-col md:flex-row top-[-100%] sm:top-[inherit]
+      <div className="grid grid-cols-4 gap-4 justify-center sticky h-20 w-full bg-faint-grey font-Hardigan font-bold text-green-blueish-dark">
+          <div className='col-span-1'>
+            <Link href="/" className="flex h-full items-center justify-center hover:text-green-dark">Home</Link>
+          </div>   
+          <div className='col-span-2'>
+          </div>  
+          <div className="col-span-1">  
+           <div className="absolute sm:flex h-full items-center justify-center gap-2 flex-col md:flex-row top-[-100%] sm:top-[inherit]
                             ease-in duration-300"
                  id="menu">
               <Link className="hover:text-green-dark" href="/threejspage">Blog</Link>
               <Link className="hover:text-green-dark" href="/about">About</Link>
               <Link className="hover:text-green-dark" href="/contact">Contact</Link>             
             </div>
-            <div className='sm:hidden items-center'>
+            <div className='sm:hidden flex h-full justify-center items-center'>
               <NavBarIcon icon={<FaBars size="20" />}/>
             </div>
-          </div>
-        </div>
+          </div>                                    
       </div>
       </>
     );
