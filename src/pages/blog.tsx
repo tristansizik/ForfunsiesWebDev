823d2f71c6a78b9318d9fcf2aa57fca679e062ff
tristansizik <tristan.sizik@gmail.com>
2024-01-18 17:16:19 -0800

    updated my blog :)

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,12 +1,11 @@
 import HeadInfo from '@/components/skeleton/HeadInfo'
-import ThreeJsPage from '@/components/skeleton/ThreeJsPage'
+import BlogPage from '@/components/skeleton/BlogPage'
 
 export default function Photogallery() {
   return (
     <>
         <HeadInfo/>
-        <ThreeJsPage/>
-      <p>hi</p>
+        <BlogPage/>
     </>
   )
 }
