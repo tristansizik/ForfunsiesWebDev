9c8dfc0c8e70c7586a6873b57155ecfe053c6097
Tristan Sizik <tristansizikcode@gmail.com>
2024-11-13 00:32:32 -0800

    trying to create a cleaner launchpage for home menu of website

--- src/components/NavBarHome.js
+++ src/components/NavBarHome.js
@@ -4,43 +4,41 @@ import React, { useEffect, useState } from 'react'
 
 const NavBarHome = (bgColor) => {
   const [click, setClick] = useState(false);
+
+    const showMenu = () => {
+      if (click) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "auto";
+      }
+      setClick(!click);
+    }
+  
     return (
       <>
-      <div className="grid grid-cols-4 gap-4 justify-center sticky h-20 w-full bg-faint-grey font-Hardigan font-bold text-green-blueish-dark">
+      <div className="grid grid-cols-4 gap-4 justify-center sticky h-20 w-full font-Hardigan font-bold text-green-blueish-dark">
           <div className='col-span-1'>
             <Link href="/" className="flex h-full items-center justify-center hover:text-green-dark">Home</Link>
           </div>   
           <div className='col-span-2'>
           </div>  
-          <div className="col-span-1">  
-           <div className="absolute sm:flex h-full items-center justify-center gap-2 flex-col md:flex-row top-[-100%] sm:top-[inherit]
-                            ease-in duration-300"
-                 id="menu">
-              <Link className="hover:text-green-dark" href="/threejspage">Blog</Link>
-              <Link className="hover:text-green-dark" href="/about">About</Link>
-              <Link className="hover:text-green-dark" href="/contact">Contact</Link>             
+          <div className="col-span-1 font-bold text-green-blueish-dark">  
+            <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+              <Link className="hidden hover:text-green-dark sm:block" href="/threejspage">Blog</Link>
+              <Link className="hidden hover:text-green-dark" href="/about">About</Link>
+              <Link className="hidden hover:text-green-dark" href="/contact">Contact</Link>             
             </div>
             <div className='sm:hidden flex h-full justify-center items-center'>
-              <NavBarIcon icon={<FaBars size="20" />}/>
+              <button onClick={showMenu} className="navbar-icon group"><FaBars size="20" /></button>
+            </div>
+            <div className="fixed left-0 top-0 z-10 h-full w-full transform opacity-95 duration-300
+                  ease-in-out translate-x-0">
+
             </div>
           </div>                                    
       </div>
       </>
     );
-  }
-
-  const NavBarIcon = ({icon}) => {
-    return (
-    <button onClick={ClickEvent} className="navbar-icon group">
-      {icon}
-    </button>
-    );
-  }
-
-  const ClickEvent = () => {
-    console.log("I've been clicked");
-    let menu = document.getElementById("menu");
-    menu.classList.toggle("top-[100]");
   }
 
   export default NavBarHome;
--- src/components/skeleton/Home.tsx
+++ src/components/skeleton/Home.tsx
@@ -6,18 +6,23 @@ import ThreeJS from '@/components/sketches/ThreeJsSketch'
 
 export default function BodyInfo() {
     return(
-      <main className= "min-h-screen flex flex-col items-center bg-black animate-fade-in-p7">
-        <NavBarHome/>         
+      <main className="">
+        <section className= "flex items-center justify-center h-screen w-full bg-black text-white animate-fade-in-p7 grid grid-flow-row grid-col-1">
+          <NavBarHome/>         
         
-        <div className="relative h-1/2 w-full inset-y-0 my-8 bg-rusty-black">
-        <div className="flex text-green-blueish-dark font-Hardigan sm:text-5xl text-xl font-extrabold items-center justify-center">Tristan&apos;s Website</div>          
-        <div className = "w-full place-content-center ">
-          <ThreeJS/>
-        </div>         
-        </div>
+        
+          <div className="relative h-1/2 w-full inset-y-0 my-8">
+            <div className="flex text-green-blueish-dark font-Hardigan sm:text-5xl text-xl font-extrabold items-center justify-center">Tristan&apos;s Website</div>          
+            <div className = "w-full place-content-center ">
+            <ThreeJS/>
+            </div>         
+          </div>
                 
-        <FootBar/>           
+          <FootBar/>           
+          </section>
       </main>
+      
+      
     )
 }
 
