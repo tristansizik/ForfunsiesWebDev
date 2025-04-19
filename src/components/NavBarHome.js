3db014afe022b99eb581dd3f91bb96b383dec8f9
Tristan Sizik <tristansizikcode@gmail.com>
2025-04-19 08:38:12 -0700

    I'm so proud to say that I've been working on and improving my css skill :), was quite frustrating tho. anyways, updated my homepage!

--- src/components/NavBarHome.js
+++ src/components/NavBarHome.js
@@ -16,25 +16,31 @@ const NavBarHome = (bgColor) => {
   
     return (
       <>
-      <div className="grid grid-cols-4 gap-4 justify-center sticky h-20 w-full font-Hardigan font-bold text-green-blueish-dark">
-          <div className='col-span-1'>
+      <div className="flex flex-row justify-between items-center sticky h-20 w-2/3 font-Hardigan font-bold text-green-blueish-dark">
+          <div className='left h-full me-[100px] w-[120px]'>
             <Link href="/" className="flex h-full items-center justify-center hover:text-green-dark">Home</Link>
-          </div>   
-          <div className='col-span-2'>
-          </div>  
-          <div className="col-span-1 font-bold text-green-blueish-dark">  
-            <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-              <Link className="hidden hover:text-green-dark sm:block" href="/threejspage">Blog</Link>
-              <Link className="hidden hover:text-green-dark" href="/about">About</Link>
-              <Link className="hidden hover:text-green-dark" href="/contact">Contact</Link>             
+          </div>    
+          <div className="right grid grid-cols-7 right items-center justify-center h-full w-[360px] font-bold text-green-blueish-dark">  
+            <div className='flex items-center justify-center col-span-2'>
+              <Link className="flex items-center justify-center col-span-1 hidden hover:text-green-dark sm:block" href="/threejspage">Blog</Link>
             </div>
+            <div className='flex items-center justify-center col-span-2'>
+              <Link className="flex items-center justify-center col-span-1 hidden hover:text-green-dark sm:block" href="/about">About</Link>
+            </div>
+            <div className='flex items-center justify-center col-span-2'>
+              <Link className="flex items-center justify-center col-span-1 hidden hover:text-green-dark sm:block" href="/contact">Contact</Link>
+            </div>
+            <div className='flex items-center justify-center col-span-1'>
+            <button onClick={showMenu} className="navbar-icon group"><FaBars size="20" /></button>
+            </div>
+            
             <div className='sm:hidden flex h-full justify-center items-center'>
               <button onClick={showMenu} className="navbar-icon group"><FaBars size="20" /></button>
             </div>
-            <div className="fixed left-0 top-0 z-10 h-full w-full transform opacity-95 duration-300
+            {/* <div className="fixed left-0 top-0 z-10 h-full w-full transform opacity-95 duration-300
                   ease-in-out translate-x-0">
 
-            </div>
+            </div> */}
           </div>                                    
       </div>
       </>
--- src/components/skeleton/Home.tsx
+++ src/components/skeleton/Home.tsx
@@ -6,23 +6,17 @@ import ThreeJS from '@/components/sketches/ThreeJsSketch'
 
 export default function BodyInfo() {
     return(
-      <main className="">
-        <section className= "flex items-center justify-center h-screen w-full bg-black text-white animate-fade-in-p7 grid grid-flow-row grid-col-1">
-          <NavBarHome/>         
-        
-        
-          <div className="relative h-1/2 w-full inset-y-0 my-8">
-            <div className="flex text-green-blueish-dark font-Hardigan sm:text-5xl text-xl font-extrabold items-center justify-center">Tristan&apos;s Website</div>          
-            <div className = "w-full place-content-center ">
-            <ThreeJS/>
-            </div>         
+      <main className="flex flex-col justify-center items-center h-screen w-full pt-24 px-4 bg-[#de9151] ">
+        <NavBarHome></NavBarHome>    
+          <div className= "flex flex-row h-1/2 w-2/3 px-4 py-4 mb-10 bg-[#f34213] text-center">
+            <img className=" h-full mx-10 bg-[white] z-20 mb-2" src='./images/1.jpg' alt="Me :)" />
+            <div className="flex flex-col justify-center items-center w-2/3 h-full mx-10  z-10 ">
+              <h1 className="font-Helvetica font-bold text-5xl text-[#2e2e3a]">TRISTAN SIZIK</h1>
+              <p className='font-Helvetica font-bold text-xl text-[#2e2e3a]'>A passionate, eclectic creative sharing his adventures in this cozy corner of the internet</p>
+            </div>
           </div>
-                
-          <FootBar/>           
-          </section>
+        <FootBar></FootBar>
       </main>
-      
-      
     )
 }
 
