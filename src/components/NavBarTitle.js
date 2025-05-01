898bdae4edb03177759ea9fa87f78703ff60a082
Tristan Sizik <tristansizikcode@gmail.com>
2025-04-30 23:36:21 -0700

    fixed the nav bar for other screens outside of home screen + removed the z values that I had in some items in Home

--- src/components/NavBarTitle.js
+++ src/components/NavBarTitle.js
@@ -17,23 +17,23 @@ const NavBarTitle = (bgColor) => {
     return (
       <>
       <div className="flex flex-row justify-between items-center h-20 w-full font-Helvetica font-bold text-dark-grayish-blue bg-coral">
-          <div className='left h-full me-[100px] w-[120px]'>
+          <div className='left h-full me-[100px] w-1/3 md:w-[120px]'>
             <Link href="/" className="flex h-full items-center justify-center hover:text-green-dark">Home</Link>
           </div> 
-          <div className="flex flex-row w-full h-full justify-center items-center text-2xl font-Helvetica font-bold">
+          <div className="flex flex-row w-2/3 h-full justify-center items-center text-2xl font-Helvetica font-bold">
             TRISTAN&apos;S WEBSITE
           </div>  
           <div className="right grid grid-cols-7 right items-center justify-center h-full w-[360px] font-bold text-dark-grayish-blue">  
-            <div className='flex items-center justify-center col-span-2'>
+            <div className='md:flex items-center justify-center col-span-2 hidden'>
               <Link className="flex items-center justify-center col-span-1 hidden hover:text-green-dark sm:block" href="/portfolio">Portfolio</Link>
             </div>
-            <div className='flex items-center justify-center col-span-2'>
+            <div className='md:flex items-center justify-center col-span-2 hidden'>
               <Link className="flex items-center justify-center col-span-1 hidden hover:text-green-dark sm:block" href="/about">About</Link>
             </div>
-            <div className='flex items-center justify-center col-span-2'>
+            <div className='md:flex items-center justify-center col-span-2 hidden'>
               <Link className="flex items-center justify-center col-span-1 hidden hover:text-green-dark sm:block" href="/contact">Contact</Link>
             </div>
-            <div className='flex items-center justify-center col-span-1'>
+            <div className='flex items-center justify-center md:col-span-1 col-span-7 md:hidden'>
               <OffCampusMenu></OffCampusMenu>  
             </div>
           </div>    
--- src/components/skeleton/Home.tsx
+++ src/components/skeleton/Home.tsx
@@ -9,8 +9,8 @@ export default function BodyInfo() {
       <main className="flex flex-col justify-center items-center h-screen w-full pt-24 px-4 bg-coral ">
         <NavBarHome></NavBarHome>    
         <div className= "flex md:flex-row flex-col h-4/5 md:h-1/2 w-2/3 px-4 py-4 md:mb-10 bg-vivid-red text-center">
-          <img className=" mx-10 max-w-full z-20 mb-2" src='./images/1.jpg' alt="Me :)" />
-          <div className="flex flex-col justify-center items-center w-2/3 h-full mx-10  z-10 ">
+          <img className=" mx-10 max-w-full mb-2" src='./images/1.jpg' alt="Me :)" />
+          <div className="flex flex-col justify-center items-center w-2/3 h-full mx-10">
             <h1 className="font-Helvetica font-bold text-lg md:text-6xl text-dark-grayish-blue">TRISTAN SIZIK –</h1>
             <p className='font-Helvetica font-bold text-sm md:text-xl text-dark-grayish-blue'>A passionate, eclectic creative sharing his adventures in this cozy digital nook</p>
             <p className='font-Helvetica font-bold hidden md:block md:text-sm text-dark-grayish-blue mt-5'>Currently In My Orbit:<br/>Event Production<br/>Music Production<br/>DJing<br/>Running </p>
